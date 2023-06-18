3e35b6e4924fbe0f777292afd3e4b29d538a3eca
Emad_HP_Home <e.a.darwich@gmail.com>
2023-06-19 00:50:20 +0200

    feat(streamlit): :fire: Download text as csv

--- app.py
+++ app.py
@@ -5,8 +5,15 @@ from datetime import datetime
 from pytube import YouTube
 from moviepy.editor import VideoFileClip
 import whisper
+import pandas as pd
+import pyperclip
 
-st.header('Youtube to text')
+st.set_page_config(layout="wide",
+                   page_title='Transcribe YouTube Video',
+                   page_icon=':video_camera:')
+
+
+st.header('Transcribe YouTube Video :video_camera:')
 
 
 
@@ -31,7 +38,6 @@ else:
     print("There are not enough matching directories to remove.")
 
 
-st.write('............ check existing folders : OK')
 
 
 
@@ -61,24 +67,6 @@ st.write('............ check existing folders : OK')
 
 
 
-# Get the current date and time
-now = datetime.now()
-
-# Create a new folder with the current hour
-current_folder = f"Folder_{now.strftime('%Y%m%d_%H%M%S')}"
-os.makedirs(current_folder)
-
-# Create the subdirectories within the current folder
-mp4_directory = os.path.join(current_folder, 'media', 'mp4')
-mp3_directory = os.path.join(current_folder, 'media', 'mp3')
-txt_directory = os.path.join(current_folder, 'media', 'txt')
-
-# Create the subdirectories
-os.makedirs(mp4_directory)
-os.makedirs(mp3_directory)
-os.makedirs(txt_directory)
-
-st.write('............ Create a new folder with the current hour : OK')
 
 
 
@@ -127,9 +115,35 @@ st.write('............ Create a new folder with the current hour : OK')
 
 # Specify the YouTube video URL
 video_url = st.text_input('Youtube link', 'https://www.youtube.com/watch?v=8Zx04h24uBs&ab_channel=LexClips')
-youtube_button = st.button('Click Here')
+youtube_button = st.button('Transcribe')
 
 if youtube_button:
+
+    
+
+
+
+    # Get the current date and time
+    now = datetime.now()
+
+    # Create a new folder with the current hour
+    current_folder = f"Folder_{now.strftime('%Y%m%d_%H%M%S')}"
+    os.makedirs(current_folder)
+
+    # Create the subdirectories within the current folder
+    mp4_directory = os.path.join(current_folder, 'media', 'mp4')
+    mp3_directory = os.path.join(current_folder, 'media', 'mp3')
+    txt_directory = os.path.join(current_folder, 'media', 'txt')
+
+    # Create the subdirectories
+    os.makedirs(mp4_directory)
+    os.makedirs(mp3_directory)
+    os.makedirs(txt_directory)
+
+
+
+
+
     # Create a YouTube object
     yt = YouTube(video_url)
 
@@ -138,7 +152,7 @@ if youtube_button:
 
     # Download the video
     stream.download(mp4_directory)
-    st.write('............ youtube downloaded : OK')
+    st.write('YouTube download ...................... OK')
 
 
 
@@ -176,7 +190,6 @@ if youtube_button:
         # Rename the file
         os.rename(current_path, new_path)
 
-        st.write('............ video renamed : OK')
 
 
 
@@ -214,7 +227,7 @@ if youtube_button:
     video.close()
     audio.close()
 
-    st.write('............ convert mp4 to mp3 : OK')
+    st.write('Convert mp4 to mp3..................... OK')
 
 
 
@@ -242,6 +255,9 @@ if youtube_button:
 
 
 
+
+    col1, col2 = st.columns(2)
+    
 
     
 
@@ -250,8 +266,9 @@ if youtube_button:
     print(result["text"])
 
 
-    st.write('............ get text from mp3 : OK')
-
+    
+    col2.audio(f"{mp3_directory}/my_audio.mp3")
+    col2.video(video_url)
 
 
 
@@ -278,8 +295,7 @@ if youtube_button:
         # Write the data to the file
         file.write(result['text'])
 
-    print("Data saved to", txt_path)
-    st.write('............ text saved : OK')
+    # print("Data saved to", txt_path)
 
 
 
@@ -291,5 +307,26 @@ if youtube_button:
 
 
 
-    st.markdown(result['text'])
+
+
+
+
+    col1.info(f"Detected language: {result['language']}")
+
+
+    
+    for segment in result['segments']:
+        start = round(float(segment['start']),2)
+        end = round(float(segment['end']),2)
+        text = segment['text']
+        col1.markdown(f"""[{start} : {end}] : {text}""")
+
+    
+
+
+                                                                                    
+    st.download_button('Download text as csv', result['text'])
+
+
+
     

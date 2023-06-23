4b06564610feeab5a6ac4eb59cb15197bf5c5dee
Emad_HP_Home <e.a.darwich@gmail.com>
2023-06-24 00:35:46 +0200

    feat:

--- pages/Transcribe Youtube.py
+++ pages/Transcribe Youtube.py
@@ -75,11 +75,11 @@ if youtube_button or st.session_state.keep_graphics:
 
                     
 
-            with st.spinner("Transcribe YouTube Video ... "):
-                    col2.audio(f"{mp3_directory}/my_audio.mp3")
-                    col2.video(video_url)
-
             
+            col2.audio(f"{mp3_directory}/my_audio.mp3")
+            col2.video(video_url)
+
+            with st.spinner("Transcribe YouTube Video ... "):
                     result = transcribe_mp3(mp3_directory, "my_audio") 
     
                     txt_path = f"{txt_directory}/output.txt" 

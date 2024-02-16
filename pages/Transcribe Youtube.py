9e4c2fed01f272ba81345d1ac6ad1159dc37cbe3
Steven Delval <100761253+StevenDelval@users.noreply.github.com>
2024-02-16 16:06:46 +0100

    fix : fix bug when empty text

--- pages/Transcribe Youtube.py
+++ pages/Transcribe Youtube.py
@@ -135,37 +135,38 @@ if youtube_button or st.session_state.keep_graphics:
                         with open(txt_path, 'w') as file:
                             # Write the data to the file
                             file.write(result['text'])
+                try:
+                    WebVTT = "WEBVTT"
+                    with col1:
+                        st.info(f"Detected language: {result['language']}")
 
-                WebVTT = "WEBVTT"
-                with col1:
-                    st.info(f"Detected language: {result['language']}")
 
+                        
+                        for segment in result['segments']:
+                            start = round(float(segment['start']),2)
+                            end = round(float(segment['end']),2)
+                            text = segment['text']
+                            WebVTT += f""" \n[{start} : {end}] : {text}"""
+                        
 
+                        user_text = st.text_area("The Complete Text",result['text'], height=450)
                     
-                    for segment in result['segments']:
-                        start = round(float(segment['start']),2)
-                        end = round(float(segment['end']),2)
-                        text = segment['text']
-                        WebVTT += f""" \n[{start} : {end}] : {text}"""
-                    
+                    with st.expander("WebVTT"):
+                            st.text_area("Format Web Video Text Tracks (WebVTT)", WebVTT, height=200)
 
-                    user_text = st.text_area("The Complete Text",result['text'], height=450)
-                
-                with st.expander("WebVTT"):
-                        st.text_area("Format Web Video Text Tracks (WebVTT)", WebVTT, height=200)
+                        
 
-                    
-
-                col2.video(video_url)
-                                                                                
-                #st.download_button('Download text as csv', result['text'])
-                st.download_button(
-                        label=f"Download as txt",
-                        data=result['text'],
-                        file_name=f'Transcribe YouTube Video {datetime.now()}.txt',
-                        mime='text/plain'
-                    )
-        
+                    col2.video(video_url)
+                                                                                    
+                    #st.download_button('Download text as csv', result['text'])
+                    st.download_button(
+                            label=f"Download as txt",
+                            data=result['text'],
+                            file_name=f'Transcribe YouTube Video {datetime.now()}.txt',
+                            mime='text/plain'
+                        )
+                except:
+                    st.write("Pas de texte détecté")
 
         else:
                 
@@ -179,65 +180,67 @@ if youtube_button or st.session_state.keep_graphics:
                 # Calculate the number of 3-minute segments
                 num_segments = int(duration // 180) + 1
                 result_text = ''
-                WebVTT = "WEBVTT"
-                for i in range(num_segments):
+                try:
+                    WebVTT = "WEBVTT"
+                    for i in range(num_segments):
+                        
+                            # Set the start and end times for the segment
+                            start_time = i * 180  # 3 minutes
+                            end_time = min((i + 1) * 180, duration)  # 3 minutes or remainder of the video
+                            with st.spinner(f"Please wait ..."):
+                                # Extract the segment and convert to MP3
+                                segment = video.subclip(start_time, end_time)
+                                output_filename = f"{mp3_directory}/segment_{i + 1}.mp3"
+                                segment.audio.write_audiofile(output_filename)
+
+                                col2.audio(f"{mp3_directory}/segment_{i + 1}.mp3")
+
+
+                            with st.spinner(f"Transcribe from : {round(start_time/60,2)} min. to {round(end_time/60,2)} min."):
+                                result = transcribe_mp3(mp3_directory, f"segment_{i + 1}")
+                            col1.write(f"Transcribe from : {round(start_time/60,2)} min. to {round(end_time/60,2)} min. : OK")
+                            for segment in result['segments']:
+                                start = round(float(segment['start']),2)
+                                end = round(float(segment['end']),2)
+                                text = segment['text']
+                                #col1.markdown(f"""[{start} : {end}] : {text}""")
+                                WebVTT += f""" \n[{start} : {end}] : {text}"""
+
+
+                            result_text = result_text + result['text'] + " "
+
+                    with col1:
+                        st.info(f"Detected language: {result['language']}")
+                        
                     
-                        # Set the start and end times for the segment
-                        start_time = i * 180  # 3 minutes
-                        end_time = min((i + 1) * 180, duration)  # 3 minutes or remainder of the video
-                        with st.spinner(f"Please wait ..."):
-                            # Extract the segment and convert to MP3
-                            segment = video.subclip(start_time, end_time)
-                            output_filename = f"{mp3_directory}/segment_{i + 1}.mp3"
-                            segment.audio.write_audiofile(output_filename)
 
-                            col2.audio(f"{mp3_directory}/segment_{i + 1}.mp3")
+                    video.close()
+                    col2.video(video_url)
 
 
-                        with st.spinner(f"Transcribe from : {round(start_time/60,2)} min. to {round(end_time/60,2)} min."):
-                            result = transcribe_mp3(mp3_directory, f"segment_{i + 1}")
-                        col1.write(f"Transcribe from : {round(start_time/60,2)} min. to {round(end_time/60,2)} min. : OK")
-                        for segment in result['segments']:
-                            start = round(float(segment['start']),2)
-                            end = round(float(segment['end']),2)
-                            text = segment['text']
-                            #col1.markdown(f"""[{start} : {end}] : {text}""")
-                            WebVTT += f""" \n[{start} : {end}] : {text}"""
+                    #concatenated_text = concatenate_txt_files(txt_directory)
+                    #st.download_button('Download text as csv', concatenated_text)
+                    result = result_text
 
+                    txt_path = f"{txt_directory}/output.txt" 
+            
+                    # Open the file in write mode
+                    with open(txt_path, 'w') as file:
+                        # Write the data to the file
+                        file.write(result)
 
-                        result_text = result_text + result['text'] + " "
-
-                with col1:
-                    st.info(f"Detected language: {result['language']}")
-                    
-                
-
-                video.close()
-                col2.video(video_url)
-
-
-                #concatenated_text = concatenate_txt_files(txt_directory)
-                #st.download_button('Download text as csv', concatenated_text)
-                result = result_text
-
-                txt_path = f"{txt_directory}/output.txt" 
-        
-                # Open the file in write mode
-                with open(txt_path, 'w') as file:
-                    # Write the data to the file
-                    file.write(result)
-
-                user_text = col1.text_area("The Complete Text",result, height=450)
-                with st.expander("WebVTT"):
-                        st.text_area("Format Web Video Text Tracks (WebVTT)", WebVTT, height=200)
-        
-                col1.download_button(
-                            label=f"Download as txt",
-                            data=result,
-                            file_name=f'Transcribe YouTube Video {datetime.now()}.txt',
-                            mime='text/plain'
-                        )
-
+                    user_text = col1.text_area("The Complete Text",result, height=450)
+                    with st.expander("WebVTT"):
+                            st.text_area("Format Web Video Text Tracks (WebVTT)", WebVTT, height=200)
+            
+                    col1.download_button(
+                                label=f"Download as txt",
+                                data=result,
+                                file_name=f'Transcribe YouTube Video {datetime.now()}.txt',
+                                mime='text/plain'
+                            )
+                except:
+                    st.write("Pas de texte détecté")
 
         txt_path = f"{txt_directory}/output.txt" 
         file = open(txt_path, "r")
@@ -246,96 +249,98 @@ if youtube_button or st.session_state.keep_graphics:
 
         # Close the file
         file.close()
-        
-        # st.text_area("The Summarized Text",sample_extractive_summarization([file_contents]), height=400)
-        # #st.text_area("The Summarized Text",file_contents, height=400)
-        # st.write()
+        try :
+            # st.text_area("The Summarized Text",sample_extractive_summarization([file_contents]), height=400)
+            # #st.text_area("The Summarized Text",file_contents, height=400)
+            # st.write()
 
-        st.markdown("---")
-        st.markdown("### Text summarization ")
-
-
-        mygrid0 = make_grid(1,2)
-        with mygrid0[0][0]:
-            with st.spinner("Extractive Summarization ..."):
-                extractive_summarization = sample_extractive_summarization([file_contents])
-            st.text_area("Extractive Summarization", extractive_summarization, height=200)
-        with mygrid0[0][1]:
-            with st.spinner("Abstractive Summarization ..."):
-                abstractive_summarization = sample_abstractive_summarization([file_contents])
-            st.text_area("Abstractive Summarization", abstractive_summarization, height=200)
-
-        st.markdown("---")
-        
+            st.markdown("---")
+            st.markdown("### Text summarization ")
 
 
-        st.markdown("""### 🇳 🇪 🇷 (Named Entity Recognition)""")
-        st.write("Named Entity Recognition (NER) is a natural language processing (NLP) task that involves identifying and classifying named entities within text into predefined categories. These entities can include names of people, organizations, locations, dates, quantities, and more as you can see in the photo.")
-        st.image("images/NER.png")
-        
-        mygrid1 = make_grid(1,2)
-        with mygrid1[0][0]:
-            with st.spinner("NER (Named Entity Recognition) ..."):
-                annotated_text(*sample_recognize_to_annotated_text([file_contents]))
+            mygrid0 = make_grid(1,2)
+            with mygrid0[0][0]:
+                with st.spinner("Extractive Summarization ..."):
+                    extractive_summarization = sample_extractive_summarization([file_contents])
+                st.text_area("Extractive Summarization", extractive_summarization, height=200)
+            with mygrid0[0][1]:
+                with st.spinner("Abstractive Summarization ..."):
+                    abstractive_summarization = sample_abstractive_summarization([file_contents])
+                st.text_area("Abstractive Summarization", abstractive_summarization, height=200)
 
-        with mygrid1[0][1]:
-            with st.spinner("DataFrame ..."):
-                dico = list_to_dict([file_contents])
-                df = pd.DataFrame(dico.items(), columns=['Entity', 'Values'])
-                
-                df['Unique Values'] = df['Values'].apply(lambda x: list(set(x)))
-                df['Number of Elements'] = df['Values'].apply(lambda x: len(x))
-                df = df.drop_duplicates(subset='Values').reset_index(drop=True)
-                df = df.drop('Values', axis=1)  # Drop the 'Values' column
-                st.dataframe(df)
-
-        # Create the bar chart using Plotly
-        fig = px.bar(df, x='Entity', y='Number of Elements', labels={'Entity': 'Entity', 'Count': 'Number of Values'})
-
-        # Set layout properties
-        fig.update_layout(
-            xaxis=dict(tickangle=0),
-            height=400  # Adjust the height as per your requirement
-        )
-
-        # Display the chart using Streamlit
-        st.plotly_chart(fig, use_container_width = True)    
-
-
-        st.markdown("---")
-        st.markdown("""### Sentiment Analysis 😃 😶 😡 """)
-        st.write("""
-        The `compound score` typically ranges from -1 to +1, where -1 indicates extremely negative sentiment, +1 indicates extremely positive sentiment, and 0 represents neutral sentiment.
-            """)
-
-        analyser = SentimentIntensityAnalyzer()
-        score = analyser.polarity_scores(file_contents)
-        polarity_vader = score['compound']
-        
-    
-
-
-        mygrid2 = make_grid(1,3)
-        with mygrid2[0][1]:
-            st.markdown(f"##### Compound Score = {polarity_vader}")
-
-        if polarity_vader > 0:
-            st.write("The sentiment of this text is likely to be positive")
-        elif polarity_vader == 0 :
-            st.write("The sentiment of this text is likely to be neutral")
-        else:
-            st.write("The sentiment of this text is likely to be negative")
+            st.markdown("---")
             
-        fig = plot_sentiment(polarity_vader)
-        # Display the plot in Streamlit
-        st.pyplot(fig)
 
-    for i in range(20):
-        st.write("")
 
-    st.markdown("""
-    ----
-    **Contact** :
+            st.markdown("""### 🇳 🇪 🇷 (Named Entity Recognition)""")
+            st.write("Named Entity Recognition (NER) is a natural language processing (NLP) task that involves identifying and classifying named entities within text into predefined categories. These entities can include names of people, organizations, locations, dates, quantities, and more as you can see in the photo.")
+            st.image("images/NER.png")
+            
+            mygrid1 = make_grid(1,2)
+            with mygrid1[0][0]:
+                with st.spinner("NER (Named Entity Recognition) ..."):
+                    annotated_text(*sample_recognize_to_annotated_text([file_contents]))
 
-    If you have any questions, suggestions or bug to report, you can contact me via my email: [e.a.darwich@gmail.com](https://www.linkedin.com/in/e-darwich/)
-    """)
+            with mygrid1[0][1]:
+                with st.spinner("DataFrame ..."):
+                    dico = list_to_dict([file_contents])
+                    df = pd.DataFrame(dico.items(), columns=['Entity', 'Values'])
+                    
+                    df['Unique Values'] = df['Values'].apply(lambda x: list(set(x)))
+                    df['Number of Elements'] = df['Values'].apply(lambda x: len(x))
+                    df = df.drop_duplicates(subset='Values').reset_index(drop=True)
+                    df = df.drop('Values', axis=1)  # Drop the 'Values' column
+                    st.dataframe(df)
+
+            # Create the bar chart using Plotly
+            fig = px.bar(df, x='Entity', y='Number of Elements', labels={'Entity': 'Entity', 'Count': 'Number of Values'})
+
+            # Set layout properties
+            fig.update_layout(
+                xaxis=dict(tickangle=0),
+                height=400  # Adjust the height as per your requirement
+            )
+
+            # Display the chart using Streamlit
+            st.plotly_chart(fig, use_container_width = True)    
+
+
+            st.markdown("---")
+            st.markdown("""### Sentiment Analysis 😃 😶 😡 """)
+            st.write("""
+            The `compound score` typically ranges from -1 to +1, where -1 indicates extremely negative sentiment, +1 indicates extremely positive sentiment, and 0 represents neutral sentiment.
+                """)
+
+            analyser = SentimentIntensityAnalyzer()
+            score = analyser.polarity_scores(file_contents)
+            polarity_vader = score['compound']
+            
+        
+
+
+            mygrid2 = make_grid(1,3)
+            with mygrid2[0][1]:
+                st.markdown(f"##### Compound Score = {polarity_vader}")
+
+            if polarity_vader > 0:
+                st.write("The sentiment of this text is likely to be positive")
+            elif polarity_vader == 0 :
+                st.write("The sentiment of this text is likely to be neutral")
+            else:
+                st.write("The sentiment of this text is likely to be negative")
+                
+            fig = plot_sentiment(polarity_vader)
+            # Display the plot in Streamlit
+            st.pyplot(fig)
+        except:
+                st.write("Pas de texte détecté")
+        for i in range(20):
+            st.write("")
+
+        st.markdown("""
+        ----
+        **Contact** :
+
+        If you have any questions, suggestions or bug to report, you can contact me via my email: [e.a.darwich@gmail.com](https://www.linkedin.com/in/e-darwich/)
+        """)
+    
